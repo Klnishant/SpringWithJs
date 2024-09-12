d0225db8f82577218f88785d535e5be5d2c345f7
Klnishant <nishantkaushal075@gmail.com>
2024-09-12 16:22:48 +0530

    Project setup for SpringWithJs

---
+++ SpringWithJs/src/main/java/com/example/SpringWithJs/SpringWithJsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWithJs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithJsApplication.class, args);
+	}
+
+}
---
+++ SpringWithJs/src/test/java/com/example/SpringWithJs/SpringWithJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWithJs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
